7516ea32748a84334858dcc7bdab7cf68cb9071d
alperen-sakin <azapkaan@gmail.com>
2025-11-16 05:11:59 +0100

    Add custom fonts, colors, and assets to project
    Introduced Poppins font family and updated typography to use it. Added new color definitions for categories and medals. Included multiple drawable icons and font files. Updated build configuration to support Google and Firebase authentication, Detekt static analysis, and preloaded fonts. Minor theme and manifest adjustments to integrate new resources.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,10 +2,12 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.detekt)
 
     kotlin("kapt")
     alias(libs.plugins.hilt)
 
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -37,8 +39,10 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-    kotlinOptions {
-        jvmTarget = "11"
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
+        }
     }
     buildFeatures {
         compose = true
@@ -54,6 +58,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.play.services.auth)
+    implementation(libs.googleid)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -61,6 +67,9 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    detektPlugins(libs.detekt)
+
 
     // Hilt - Dependency Injection
     implementation(libs.hilt.android)
@@ -71,5 +80,27 @@ dependencies {
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.hilt.navigation.compose)
 
+    //fireBase
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.auth)
+    implementation(libs.firebase.firestore)
 
+    // androidx.credentials
+    implementation(libs.androidx.credentials)
+// Google ile Giriş gibi federated servisleri kullanmak için
+    implementation(libs.androidx.credentials.play.services.auth)
+
+
+}
+
+detekt {
+    config.setFrom(file("$rootDir/detekt/detektConfig.yml"))
+    source.from(files("src/main/kotlin"))
+    parallel = true
+    autoCorrect = true
+    buildUponDefaultConfig = true
+}
+
+tasks.withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
+    jvmTarget = "11"
 }
--- app/src/main/java/com/example/quizapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/quizapp/ui/theme/Color.kt
@@ -9,3 +9,24 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val BackgroundColor = Color(0xFFEFF0F3)
+
+val QueenPink = Color(0xffE7D3D3)
+val Maroon = Color(0xffB9375D)
+val IndianREd = Color(0xffD25D5D)
+
+// categoryColors
+val CategoryGeneralColor = Color(0xff43A047)
+val CategorySportColor = Color(0xffF57C00)
+val CategoryScienceColor = Color(0xff1976D2)
+val CategoryTechnologyColor = Color(0xff546E7A)
+val CategoryGeographyColor = Color(0xff0288D1)
+val CategoryHistoryColor = Color(0xff7B1FA2)
+
+val GoldColor = Color(0xffFFC107)
+val SilverColor = Color(0xffC0C0C0)
+val BronzeColor = Color(0xffCD7F32)
+
+val SelectedContainerColor = Color(0xffABD1C6)
+val SelectedColor = Color(0xff004643)
