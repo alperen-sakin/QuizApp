f2454f196172975d633a2e215ebd76e160ae0752
alperen-sakin <azapkaan@gmail.com>
2025-11-16 05:27:36 +0100

    feat: Add Splash Screen and update dependencies
    Adds the Core Splashscreen API to implement a splash screen.
    Updates the Firebase BOM to version 34.6.0.
    Includes the extended Material Icons dependency.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,6 +67,9 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    implementation(libs.androidx.material.icons.extended)
+
+    implementation(libs.androidx.core.splashscreen)
 
     detektPlugins(libs.detekt)
 
